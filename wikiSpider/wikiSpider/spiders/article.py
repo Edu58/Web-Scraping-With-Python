4b9a07fcbcc252b0ab76d7032e748f842ef99b90
“Edu58” <edumuriithi58@gmail.com>
2022-07-20 16:25:27 +0300

    adds article.py docstrings

--- wikiSpider/wikiSpider/spiders/article.py
+++ wikiSpider/wikiSpider/spiders/article.py
@@ -5,6 +5,13 @@ class ArticleSpider(scrapy.Spider):
     name = "article"
 
     def start_requests(self):
+        """_summary_
+            use of start_requests()
+        Returns:
+            response(list): _description_
+
+            start_requests is a Scrapy-defined entry point to the program used to generate Request objects that Scrapy uses to crawl the website.
+        """
         urls = [
             "https://en.wikipedia.org/wiki/Programming_paradigm",
             "https://en.wikipedia.org/wiki/Functional_programming",
@@ -14,6 +21,12 @@ class ArticleSpider(scrapy.Spider):
         return [scrapy.Request(url=url, callback=self.parse) for url in urls]
 
     def parse(self, response):
+        """_summary_
+            use of parse()
+        Args:
+            response (None): _description_
+            parse is a callback function defined by the user, and is passed to the Request object with callback=self.parse.
+        """
         url = response.url
         title = response.css('h1::text').extract_first()
         print(f'URL is: {url}')
